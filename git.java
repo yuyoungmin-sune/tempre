9db6ce0e646d325ab184bbbc816bc110c34c9bd7
yuyoungmin <yuyoungmin@gmail.com>
2020-08-27 17:55:13 +0900

    asdfasdf

---
+++ git.java
@@ -0,0 +1,58 @@
+
+
+
+
+GitHubを使用。あらかじめリポジトリを作成
+
+
+
+
+yuyoungmin-sune
+
+yuyoungmin@gmail.com
+7204bobo
+
+https://github.com/yuyoungmin-sune/tempre
+
+
+
+
+
+
+
+$ git config --global user.name "yuyoungmin"
+
+
+
+https://github.com/jquery/jquery.git
+
+
+
+git config --global user.name "yuyoungmin"
+git config --global user.email "yuyoungmin@gmail.com"
+
+
+
+
+
+
+
+
+git config --global user.name "yuyoungmin"
+git config --global user.email "yuyoungmin@gmail.com"
+
+ 
+
+
+git add
+
+
+
+
+
+
+
+
+git config --global user.name "yuyoungmin"
+git config --global user.email "yuyoungmin@gmail.com"
+
